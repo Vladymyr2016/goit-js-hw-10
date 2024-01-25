a31af1e3285feaccebaeb5f0fbed294c9ab62926
Vladymyr2016 <142669270+Vladymyr2016@users.noreply.github.com>
2024-01-25 11:57:29 +0100

    task1

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,4 +1,40 @@
-let userSelectedDate;
+import flatpickr from 'flatpickr';
+import iziToast from 'izitoast';
+import 'flatpickr/dist/flatpickr.min.css';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const startBtn = document.querySelector('button');
+const daysValue = document.querySelector('[data-days]');
+const hoursValue = document.querySelector('[data-hours]');
+const minValue = document.querySelector('[data-minutes]');
+const secValue = document.querySelector('[data-seconds]');
+class Timer {
+  constructor(convertMs, selectedDates) {
+    this.intervalId = null;
+    this.collectTime = convertMs;
+  }
+  start() {
+    this.intervalId = setInterval(() => {
+      const diff = this.selectedDates - Date.now();
+      daysValue.textContent = padStart(this.collectTime(diff).days);
+
+      hoursValue.textContent = this.collectTime(diff).hours;
+      minValue.textContent = this.collectTime(diff).minutes;
+      secValue.textContent = this.collectTime(diff).seconds;
+    }, 1000);
+  }
+  cleanInterval() {
+    clearInterval(this.intervalId);
+  }
+}
+
+startBtn.addEventListener('click', () => {
+  timer.start();
+});
+
+function padStart(number) {
+  return number.toString().padStart(2, '0');
+}
 
 const options = {
   enableTime: true,
@@ -6,31 +42,35 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    timer.selectedDates = selectedDates[0];
+    if (timer.selectedDates < Date.now()) {
+      startBtn.disabled = true;
+      iziToast.show({
+        title: 'Hey',
+        message: 'Please choose a date in the future',
+      });
+      console.log(iziToast);
+    } else {
+      startBtn.disabled = false;
+    }
   },
 };
 
-function flatpickr(selector, options) {}
+const input = document.querySelector('#datetime-picker');
+const fp = flatpickr('#datetime-picker', options);
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
-
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
+
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
 }
 
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+const timer = new Timer(convertMs);
